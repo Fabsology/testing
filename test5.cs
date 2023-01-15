fcb868df059dcec858bcbbad92d6795526a2dcd3
Fabsology <102152781+Fabsology@users.noreply.github.com>
2023-01-15 23:49:34 +0100

    Add files via upload

---
+++ test5.cs
@@ -0,0 +1,10 @@
+// Hello World! program
+namespace HelloWorld
+{
+    public static class Hello {         
+        public static void Hay()
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
